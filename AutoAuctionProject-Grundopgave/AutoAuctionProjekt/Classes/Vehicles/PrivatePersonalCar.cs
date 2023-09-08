03040a761295607a887291506f31f00b6882e7d1
laur887b@gmail.com <laur887b@gmail.com>
2023-09-08 12:35:24 +0200

    #57 V19 - Constructor for PrivatePersonalCar

--- AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/Vehicles/PrivatePersonalCar.cs
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/Vehicles/PrivatePersonalCar.cs
@@ -21,9 +21,12 @@ namespace AutoAuctionProjekt.Classes
             bool hasIsofixFittings)
             : base(name, km, registrationNumber, year, newPrice, hasTowbar, engineSize, kmPerLiter, fuelType, numberOfSeat, trunkDimentions)
         {
-            //TODO: V19 - PrivatePersonalCar constructor. DriversLicense should be 'B'
+
+
+            HasIsofixFittings = hasIsofixFittings;
+
+
             //TODO: V20 - Add to database and set ID
-            throw new NotImplementedException();
         }
         /// <summary>
         /// Isofix Fittings proberty
