307ef2d4e682e550eb6cc48dff7a21cd6d847a18
laur887b@gmail.com <laur887b@gmail.com>
2023-09-26 09:48:06 +0200

    #40 U7- Added to constructor for CorporateUser

--- AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/CorporateUser.cs
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/CorporateUser.cs
@@ -6,9 +6,11 @@ namespace AutoAuctionProjekt.Classes
 {
     public class CorporateUser : User
     {
-        public CorporateUser(string userName, string password, uint zipCode, uint cvrNummer, decimal credit) : base(userName, password, zipCode)
+        public CorporateUser(string userName, bool isCorporate, double balance, uint zipCode, uint cvrNummer, decimal credit) : base(userName, isCorporate, balance, zipCode)
         {
-            //TODO: U7 - Set constructor
+            CVRNumber = cvrNummer;
+            Credit = credit;
+
             //TODO: U8 - Add to database and set ID
             throw new NotImplementedException();
         }
