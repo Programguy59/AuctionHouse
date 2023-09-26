167f075d3fd8434164883c3aaba3f39f2594bf69
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-09-26 10:39:17 +0200

    D9 - Created Auction Table

---
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/SqlQurries/CreateTables/CreateTableAuction.sql
@@ -0,0 +1,8 @@
+use AuctionHouse;
+
+CREATE TABLE Auctions ( 
+    Id int PRIMARY KEY not null,
+	vehicleId int  FOREIGN KEY (vehicleId) REFERENCES Vehicle(id), 
+	userName varchar(128) FOREIGN KEY (userName) REFERENCES Users(userName),
+	minimumPrice decimal not null
+)
