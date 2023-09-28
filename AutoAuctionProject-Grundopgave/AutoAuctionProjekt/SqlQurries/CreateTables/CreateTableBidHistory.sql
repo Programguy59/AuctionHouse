24578b98164e071f23c5cfdf7476363bedb3e176
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-09-28 13:52:06 +0200

    D? - BidHistory Not null added

--- AutoAuctionProject-Grundopgave/AutoAuctionProjekt/SqlQurries/CreateTables/CreateTableBidHistory.sql
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/SqlQurries/CreateTables/CreateTableBidHistory.sql
@@ -4,6 +4,6 @@ CREATE TABLE BidHistory (
     id int PRIMARY KEY IDENTITY not null,
 	date datetime not null,
 	bidAmount decimal not null,
-	userName varchar(128) FOREIGN KEY (userName) REFERENCES Users(userName),
-	auctionId int  FOREIGN KEY (auctionId) REFERENCES Auctions(id),
+	userName varchar(128) FOREIGN KEY (userName) REFERENCES Users(userName) not null,
+	auctionId int  FOREIGN KEY (auctionId) REFERENCES Auctions(id) not null,
 )
