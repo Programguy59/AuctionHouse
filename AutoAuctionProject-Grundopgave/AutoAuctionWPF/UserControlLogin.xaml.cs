9756f2188f9612123847ef9dda85a58304ffec5e
casper madsen <casp589b@elev.techcollege.dk>
2023-09-29 08:21:45 +0200

    g1 #59 "made a test connection to the database"

--- AutoAuctionProject-Grundopgave/AutoAuctionWPF/UserControlLogin.xaml.cs
+++ AutoAuctionProject-Grundopgave/AutoAuctionWPF/UserControlLogin.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using AutoAuctionProjekt.Util;
+
 
 namespace AutoAuctionWPF;
 
@@ -6,16 +9,32 @@ public partial class UserControlLogin : UserControl
 {
     public UserControlLogin()
     {
-        InitializeComponent();  
+        InitializeComponent();
         this.DataContext = this;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    string _userName;
+    string _password;
+
+    private void Login_OnClick(object sender, RoutedEventArgs e)
+    {
+        Constants.Sql.User = UserNameBox.Text;
+        Constants.Sql.Password = PasswordBox.Password;
+        if (string.IsNullOrEmpty(UserNameBox.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+        {
+            MessageBox.Show("Please enter a username and password");
+        }
+        else
+        {
+            DatabaseServer.ExecuteQuery("SELECT * FROM Users");
+            var test = DatabaseServer.FetchVehicle(8);
+            MessageBox.Show(test.ToString());
+                MessageBox.Show("Login successful");
+        }
+        
+        
+
+    }
+
 }
+    
