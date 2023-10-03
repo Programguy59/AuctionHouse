5c2da571d73d9c201e6e9caa8aa5a079ed94cf2b
laur887b@gmail.com <laur887b@gmail.com>
2023-10-03 13:00:09 +0200

    ? - Added Static CorporateUser class

---
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/Vehicles/Database/StaticCorporateUser.cs
@@ -0,0 +1,24 @@
+﻿using AutoAuctionProjekt.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCorporateUserProjekt.Classes.Vehicles.Database
+{
+    public static partial class Database
+    {
+        public static readonly List<CorporateUser> CorporateUsers = new();
+
+        public static CorporateUser? GetCorporateUserById(string userName)
+        {
+            return CorporateUsers.FirstOrDefault(corporateUser => corporateUser.UserName == userName);
+        }
+
+        public static List<CorporateUser> GetAllCorporateUsers()
+        {
+            return CorporateUsers;
+        }
+    }
+}
