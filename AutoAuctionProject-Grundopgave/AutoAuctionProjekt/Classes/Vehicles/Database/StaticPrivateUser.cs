2951b03ce67b972987d265d9150c84c79c6a7c1d
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-10-03 13:00:26 +0200

    ? - Static Private User added to local database for use in program?!?!?!?!?!?!?

---
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/Vehicles/Database/StaticPrivateUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuctionProjekt.Classes.Vehicles.Database
+{
+    public static partial class Database
+    {
+        public static readonly List<PrivateUser> privateUsers = new();
+        
+        public static PrivateUser? GetPrivateUserById(string userName)
+        {
+            return privateUsers.FirstOrDefault(PrivateUser => PrivateUser.UserName == userName);
+        }
+        public static List<PrivateUser> GetAllPrivateUsers()
+        {
+            return privateUsers;
+        }
+    }
+}
