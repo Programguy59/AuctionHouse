f0288b7a75f4ff9bcc22a4011351b337ba713e93
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-10-03 12:14:42 +0200

    D? - added FetchAuction sql script and made staticAuction partial database

---
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/Vehicles/Database/StaticAuction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuctionProjekt.Classes.Vehicles.Database
+{
+    public static partial class Database
+    {
+        public static readonly List<Auction> Auctions = new();
+
+        public static Auction? GetAuctionById(int id)
+        {
+            return Auctions.FirstOrDefault(Auction => Auction.ID == id);
+        }
+
+        public static List<Auction> GetAllAuctions()
+        {
+            return Auctions;
+        }
+    }
+}
