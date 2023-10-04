91c24f7d7474424fef39e0fe6252065bd1acfce2
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-10-04 08:41:56 +0200

    ? - added static users

---
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/Classes/Vehicles/Database/StaticUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuctionProjekt.Classes.Vehicles.Database
+{
+    public static partial class Database
+    {
+        public static readonly List<User> Users = new();
+
+        public static User? GetUserByUserName(string UserName)
+        {
+            foreach (var corpUser in Users)
+            {
+                User user = corpUser;
+                if (!Users.Contains(user))
+                {
+                    Users.Add(user);
+                }
+            }
+            foreach (var PrivteUser in Users)
+            {
+                User user = PrivteUser;
+                if (!Users.Contains(user))
+                {
+                    Users.Add(user);
+                }
+            }
+            return Buses.FirstOrDefault(bus => bus.BusID == id);
+        }
+
+        public static List<User> GetAllUsers()
+        {
+            return Users;
+        }
+    }
+}
