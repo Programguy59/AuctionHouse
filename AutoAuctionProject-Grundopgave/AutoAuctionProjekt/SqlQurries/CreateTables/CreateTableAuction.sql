a3eacc82592c466fdcb8b7b78131bb832cd483b1
Magnus Melgaard Jonge <magn8244@elev.techcollege.dk>
2023-10-04 08:58:23 +0200

    D?- Auction added isDone Column

--- AutoAuctionProject-Grundopgave/AutoAuctionProjekt/SqlQurries/CreateTables/CreateTableAuction.sql
+++ AutoAuctionProject-Grundopgave/AutoAuctionProjekt/SqlQurries/CreateTables/CreateTableAuction.sql
@@ -1,8 +1,9 @@
 use AuctionHouse;
 
-CREATE TABLE Auctions ( 
+Create TABLE Auctions ( 
     id int PRIMARY KEY IDENTITY not null,
 	vehicleId int  FOREIGN KEY (vehicleId) REFERENCES Vehicle(id), 
 	userName varchar(128) FOREIGN KEY (userName) REFERENCES Users(userName),
-	minimumPrice decimal not null
+	minimumPrice decimal not null,
+	IsDone bit not null
 )
