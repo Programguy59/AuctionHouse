caf7f312265e0d6b55145a9fab310158a6dd1dc2
casper madsen <casp589b@elev.techcollege.dk>
2023-10-05 09:33:59 +0200

    G6 #65 "Added the functionality of seller of auction"

--- AutoAuctionProject-Grundopgave/AutoAuctionWPF/SellerOfAuctionControl.xaml.cs
+++ AutoAuctionProject-Grundopgave/AutoAuctionWPF/SellerOfAuctionControl.xaml.cs
@@ -1,20 +1,34 @@
 ﻿using AutoAuctionProjekt.Classes;
 using System.Windows;
 using System.Windows.Controls;
+using AutoAuctionProjekt.Classes.Vehicles.Database;
+using AutoAuctionProjekt.Util;
 
 namespace AutoAuctionWPF;
 
 public partial class SellerOfAuctionControl : UserControl
 {
     private MainWindow mainWindow;
+    private Auction auction;
     public SellerOfAuctionControl(MainWindow main, Auction auction)
     {
         InitializeComponent();
         this.mainWindow = main;
+        this.auction = auction;
+        AuctionNameTextBlock.Text = auction.Vehicle.Name;
+        AuctionInfoTextBlock.Text = auction.StandingBid.ToString();
+
     }
 
     private void AcceptBidButton_Click(object sender, RoutedEventArgs e)
     {
-        throw new System.NotImplementedException();
+        AuctionHouse.AcceptBid(Database.GetUserByUserName(Constants.Sql.User), auction.ID);
+        mainWindow.ShowHomeScreen();
     }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        mainWindow.ShowHomeScreen();
+    }
+    
 }
